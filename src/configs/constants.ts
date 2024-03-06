597a308371ddaa7bc77949884b7c136ec18684b9
macbookair13 <danilmarc4uk.grf@gmail.com>
2024-03-06 21:37:01 +0200

    Add profile settings and adjust user authentication flow
    This update introduces a new system in profile settings for user membership status and enhanced user authentication flow. New pages for membership subscription and password change have been added. Additionally, the user sign-up form has been extended with a 'name' field and minor adjustments have been done to optimize the layout and interaction elements of the settings.

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -5,7 +5,7 @@ console.log ("env", process.env.BASE_API_URL)
 
 // export const BASE_API_URL = 'https://api.aniverse.website'
 
-export const BASE_API_URL = 'http://localhost:3301'
+export const BASE_API_URL = 'https://api.aniverse.website'
 
 
 
