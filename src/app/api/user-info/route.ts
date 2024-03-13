961a48b878d1c5b30ca03482ff6e840a30803120
macbookair13 <danilmarc4uk.grf@gmail.com>
2024-03-13 20:43:59 +0200

    Add axios and implement IP API for enhanced geo-info
    The implementation now includes axios for HTTP requests and implements IP API. The GET method in the 'user-info' route now retrieves Geo-data from an external IP API, enhancing the accuracy of the geo-information provided, especially when requests are forwarded through proxies.

--- src/app/api/user-info/route.ts
+++ src/app/api/user-info/route.ts
@@ -1,19 +1,11 @@
-import {NextRequest, NextResponse} from "next/server";
-import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest){
-  console.log (req)
+export async function GET(req: NextRequest) {
   try {
-    const clientIp = req.headers.get('CF-Connecting-IP');
-    const forwarded = req.headers.get('X-Forwarded-For')
-    console.log (forwarded)
-    const ip = clientIp ? clientIp : forwarded.split(/, /)[0]
+    // Cloudflare добавляет заголовок `CF-Connecting-IP` с реальным IP клиента
+    const ip = req.headers.get("cf-connecting-ip") || req.ip;
 
-    // get Geo information from IP
-    const resp = await axios.get(`https://ipapi.co/${ip}/json/`);
-    const geoData = resp.data;
-
-    return NextResponse.json({ geo: geoData || req.geo, ip: ip || req.ip });
+    return NextResponse.json({ geo: req.geo, ip: ip });
   } catch (e) {
     return NextResponse.json({ error: e.message }, { status: 500 });
   }
