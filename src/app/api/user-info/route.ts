7ea8972d65db99881291d9777d98c646eb80f3c7
macbookair13 <danilmarc4uk.grf@gmail.com>
2024-03-13 20:25:38 +0200

    Extract client's IP from 'X-Forwarded-For' header
    This update modifies the GET API's response in the route.ts file for 'user-info' to return not only the geo-ip data, but also the client's IP from 'X-Forwarded-For' header if it exists. This helps in accurately tracking user IP even when requests pass through proxies.

--- src/app/api/user-info/route.ts
+++ src/app/api/user-info/route.ts
@@ -4,7 +4,12 @@ import {NextRequest, NextResponse} from "next/server";
 export async function GET(req: NextRequest){
   console.log (req)
   try {
-    return NextResponse.json({ geo: req.geo, ip: req.ip});
+
+    const forwarded = req.headers.get('X-Forwarded-For')
+    console.log (forwarded)
+    const ip = forwarded.split(/, /)[0]
+
+    return NextResponse.json({ geo: req.geo, ip: ip || req.ip });
   } catch (e) {
     return NextResponse.json({ error: e.message }, { status: 500 });
   }
