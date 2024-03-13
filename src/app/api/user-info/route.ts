ab0c91b74d7328175efef6037fa57b598ecaca26
macbookair13 <danilmarc4uk.grf@gmail.com>
2024-03-13 20:17:51 +0200

    Updated GET API response in user-info route
    Modified the GET API response in the route.ts file for 'user-info', to return the geo-ip data of the user instead of a 'Hello World' message. This change allows for improved tracking of user interactions with the application.

--- src/app/api/user-info/route.ts
+++ src/app/api/user-info/route.ts
@@ -4,7 +4,7 @@ import {NextRequest, NextResponse} from "next/server";
 export async function GET(req: NextRequest){
   console.log (req)
   try {
-    return NextResponse.json({ message: "Hello World" });
+    return NextResponse.json({ geo: req.geo, ip: req.ip});
   } catch (e) {
     return NextResponse.json({ error: e.message }, { status: 500 });
   }
